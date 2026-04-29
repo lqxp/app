5ca9df035d64901563004c7cf58fe98478211a5e
Anaïs Saraiva <53227802+Kisakay@users.noreply.github.com>
2026-04-29 18:38:01 +0200

    Fix issue with permission - Android App

--- src-tauri/gen/android/app/src/main/java/com/qxp/client/MainActivity.kt
+++ src-tauri/gen/android/app/src/main/java/com/qxp/client/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.qxp.client
 
 import android.os.Bundle
+import android.webkit.WebView
 import androidx.activity.enableEdgeToEdge
 
 class MainActivity : TauriActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     enableEdgeToEdge()
     super.onCreate(savedInstanceState)
+  }
+
+  override fun onWebViewCreate(webView: WebView) {
+    super.onWebViewCreate(webView)
+    webView.settings.allowContentAccess = true
+    webView.settings.allowFileAccess = true
+    webView.settings.domStorageEnabled = true
+    webView.settings.javaScriptCanOpenWindowsAutomatically = true
+    webView.settings.mediaPlaybackRequiresUserGesture = false
   }
 }
