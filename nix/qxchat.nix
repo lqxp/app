0f90979abeec0377d27ce208a456a04c56311cb8
Anaïs Saraiva <53227802+Kisakay@users.noreply.github.com>
2026-06-18 15:17:56 +0200

    Update qxchat.nix

--- nix/qxchat.nix
+++ nix/qxchat.nix
@@ -167,7 +167,7 @@ rustPlatform.buildRustPackage {
 
         cat > client/dist/runtime-config.js <<'EOF'
 
-    window.__QXP_RUNTIME__ = {"apiBaseUrl":"https://qxp.kisakay.com","rtc":{"callsEnabled":true,"callsUnavailableReason":"","relayOnly":true,"turnCredential":"df64240e730e15fdfb75d6cff95367b95ed341bd98517544","turnUrls":["turn:turn.qxp.kisakay.com:3478?transport=udp","turn:turn.qxp.kisakay.com:3478?transport=tcp","turns:turn.qxp.kisakay.com:5349?transport=tcp"],"turnUsername":"qxp-turn"},"serverOrigin":"https://qxp.kisakay.com","wsUrl":"wss://qxp.kisakay.com/ws"};
+    window.__QXP_RUNTIME__ = {"apiBaseUrl":"https://qxch.at","rtc":{"callsEnabled":true,"callsUnavailableReason":"","relayOnly":true,"turnCredential":"df64240e730e15fdfb75d6cff95367b95ed341bd98517544","turnUrls":["turn:turn.qxp.kisakay.com:3478?transport=udp","turn:turn.qxp.kisakay.com:3478?transport=tcp","turns:turn.qxp.kisakay.com:5349?transport=tcp"],"turnUsername":"qxp-turn"},"serverOrigin":"https://qxch.at","wsUrl":"wss://qxch.at/ws"};
     EOF
   '';
 
