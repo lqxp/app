91e30cd3f64d153d7790bf70f0b85ddc46a182d1
Anaïs Saraiva <53227802+Kisakay@users.noreply.github.com>
2026-07-11 21:46:09 +0200

    Update qxchat.nix

--- nix/qxchat.nix
+++ nix/qxchat.nix
@@ -90,7 +90,6 @@ let
     gst_all_1.gst-plugins-bad
     gst_all_1.gst-plugins-ugly
     gst_all_1.gst-libav
-    gst_all_1.gst-plugin-pipewire
   ];
 
   gstPluginPath = lib.concatStringsSep ":" (map (pkg: "${pkg}/lib/gstreamer-1.0") gstPlugins);
