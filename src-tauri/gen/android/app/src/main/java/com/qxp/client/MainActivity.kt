83c14b7af926f0ed5344efb2b3397e0102c270c2
Anaïs Saraiva <53227802+Kisakay@users.noreply.github.com>
2026-07-18 11:24:05 +0200

    TilteBar On android removed

--- src-tauri/gen/android/app/src/main/java/com/qxp/client/MainActivity.kt
+++ src-tauri/gen/android/app/src/main/java/com/qxp/client/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.qxp.client
 
 import android.os.Bundle
+import android.view.Window
 import android.webkit.WebView
 import androidx.activity.enableEdgeToEdge
 
 class MainActivity : TauriActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
+    requestWindowFeature(Window.FEATURE_NO_TITLE)
     enableEdgeToEdge()
     super.onCreate(savedInstanceState)
+    actionBar?.hide()
   }
 
   override fun onWebViewCreate(webView: WebView) {
